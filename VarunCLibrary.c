57f6a43bc339785a6d23e8a7793efa4ce6197c07
Varun28Maurya <110877198+Varun28Maurya@users.noreply.github.com>
2025-01-13 22:35:15 +0530

    Add files via upload

--- VarunCLibrary.c
+++ VarunCLibrary.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 void functions()
 {
-	printf("[squareroot],[raiseto],[multiplicationtable]\n");
+	printf("[print],[squareroot],[raiseto],[multiplicationtable],[p&l],[pythagorustheorem]\n");
 }
 void square_root()
 {
@@ -57,21 +58,86 @@ void multiplication_table()
 	{
 		int multiplication_table_answer;
 		multiplication_table_answer=multiplication_table_num*i;
-		multiplication_table_start++;
 		printf("\n%d x %d = %d",multiplication_table_num,multiplication_table_start,multiplication_table_answer);
+		multiplication_table_start++;
 	}
 	printf("\n");
 }
+void pl()
+{
+	double cost_price,selling_price,total;
+	printf("\nEnter the Cost Price:");
+	scanf("%lf",&cost_price);
+	printf("\nEnter the Selling Price:");
+	scanf("%lf",&selling_price);
+	total=selling_price-cost_price;
+	if(total>0)
+	{
+		printf("\nIts a profit of amount %.6f\n",total);
+	}
+	else if(total==0)
+	{
+		printf("\nThe Selling price and Cost price is same\n");
+	}
+	else{
+		printf("\nIts a Loss of amount %.6f\n",total);
+	}
+}
+void pythagorustheorem()
+{
+	printf("\nPress 1 to find Hypotenuse\n");
+	printf("\nPress 2 to find Base\n");
+	printf("\nPress 3 to find Height\n");
+	int num;
+	double hypotenuse,base,height;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	switch(num)
+	{
+		case 1:printf("\nEnter the Base:");
+		scanf("%lf",&base);
+		printf("\nEnter the Height:");
+		scanf("%lf",&height);
+		hypotenuse=sqrt((base*base)+(height*height));
+		printf("\nThe Hypotenuse of Base %.6f and Height %.6f is %.6f\n",base,height,hypotenuse);
+		break;
+		case 2:printf("\nEnter the Hypotenuse:");
+		scanf("%lf",&hypotenuse);
+		printf("\nEnter the Height:");
+		scanf("%lf",&height);
+		base=sqrt((hypotenuse*hypotenuse)-(height*height));
+		printf("\nThe Base of Hypotenuse %.6f and Height %.6f is %.6f\n",hypotenuse,height,base);
+		break;
+		case 3:printf("\nEnter the Hypotenuse:");
+		scanf("%lf",&hypotenuse);
+		printf("\nEnter the Base:");
+		scanf("%lf",&base);
+		height=sqrt((hypotenuse*hypotenuse)-(base*base));
+		printf("\nThe Height of Hypotenuse %.6f and Base %.6f is %.6f\n",hypotenuse,base,height);
+		break;
+		default:printf("\nLooks like you have entered wrong! Please try again\n");
+		pythagorustheorem();
+	}
+}
+void print()
+{
+	char str[1000];
+	printf("\nEnter whatever you want to type:");
+	getchar();
+	scanf("%[^\n]", str);
+	printf("%s\n",str);
+}
 int main()
 {
-	printf("WELCOME TO THE VARUN C LIBRARY\n");
-	printf("This File contains alot of Functions so kindly refer to the book\n");
+	printf("---------------------------------------------WELCOME TO THE VARUN C LIBRARY---------------------------------------------\n");
+	printf("\nThis File contains alot of Functions so kindly refer to the book\n");
 	int initial_number=1;
 	while(initial_number!=0)
 	{
 		char string[100];
+		printf("\n");
 		functions();
-		printf("Enter the function name you want to choose:");
+		printf("\nEnter the function name you want to choose:");
 		scanf("%s",string);
 		if(strcmp(string,"functions")==0)
 		{
@@ -88,6 +154,18 @@ int main()
 		else if(strcmp(string,"multiplicationtable")==0)
 		{
 			multiplication_table();
+		}
+		else if(strcmp(string,"p&l")==0)
+		{
+			pl();
+		}
+		else if(strcmp(string,"pythagorustheorem")==0)
+		{
+			pythagorustheorem();
+		}
+		else if(strcmp(string,"print")==0)
+		{
+			print();
 		}
 		else
 		{
